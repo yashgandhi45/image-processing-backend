8245e50fc5947ada61487f30d1f11e8727806335
Yash Gandhi <yashgandhi4505@gmail.com>
2025-04-05 01:53:05 +0530

    Working Backend Routes

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ console.log(process.env.MONGO_URI);
 const app = express();
 app.use(express.json());
 app.use('/api', routes);
+app.use('/uploads', express.static('uploads'));
+
+
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => {
